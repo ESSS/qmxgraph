1ff7491fb76e92fdce325e2fe11127389c0a7fc8
Martin Prüsse <prusse@esss.com.br>
2017-05-29 15:28:46 -0300

    Allow to create table with cells that span over multiple columns or rows

--- tests/test_html_helpers.py
+++ tests/test_html_helpers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from qmxgraph.html import create_image_tag_str
+from qmxgraph.html import create_image_tag_str, create_table_entry
 
 
 def test_create_image_tag():
@@ -12,3 +12,19 @@ def test_image_tag_avoid_injection():
     with pytest.raises(ValueError) as exception:
         create_image_tag_str('"', 0, 0)
     assert 'source argument can not have the " character' in str(exception)
+
+
+def test_create_table_entry():
+    assert 'foobar' == create_table_entry('foobar')
+    assert 'foobar' == create_table_entry('foobar', colspan=1, rowspan=1)
+    assert {
+        'value': 'foobar',
+        'colspan': 2,
+        'rowspan': 3,
+    } == create_table_entry('foobar', colspan=2, rowspan=3)
+
+    with pytest.raises(ValueError) as exception:
+        create_table_entry('foobar', colspan=4, rowspan=-1)
+
+    error_msg = 'colspan(4) and rowspan(-1) must be non negative values'
+    assert error_msg in str(exception)
