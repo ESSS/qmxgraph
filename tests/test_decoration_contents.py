5223ddbb7cb373cd0773dff283aec3b4d910b4de
Bruno Oliveira <bruno@esss.com.br>
2017-07-13 10:56:26 -0300

    Add tests for Table.contents_after

--- tests/test_decoration_contents.py
+++ tests/test_decoration_contents.py
@@ -29,3 +29,23 @@ def test_tuple_of_validator():
     msg = ("but got (Cutlery(name='fork'), Door()) and item in index 1 is not"
            " one of the expected types")
     assert msg in str(execinfo.value)
+
+
+def test_contents_after():
+    from qmxgraph.decoration_contents import Table, TableRow, TableData
+
+    table = Table([
+        TableRow([TableData(['Cutlery'])]),
+        TableRow(['Spoons', '2']),
+        TableRow(['Knifes', '5']),
+        TableRow([TableData(['Bathroom'])]),
+        TableRow(['Soap', '2']),
+        TableRow(['Toothpaste', '1']),
+    ])
+    assert table.contents_after('Bathroom') == (
+        TableRow(['Soap', '2']),
+        TableRow(['Toothpaste', '1']),
+    )
+    assert table.contents_after('Soap') == (
+        TableRow(['Toothpaste', '1']),
+    )
