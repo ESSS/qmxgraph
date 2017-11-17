749f4822ed5ec6b8d86c1d93edced3a8176272d1
Martin Prüsse <prusse@esss.com.br>
2017-11-17 16:12:17 -0200

    Add `qmxgraph.common_testing.get_cell_ids`

--- qmxgraph/common_testing.py
+++ qmxgraph/common_testing.py
@@ -11,13 +11,24 @@ def get_cell_count(widget, filter_function):
         cells.
     :rtype: int
     """
-    count = widget.inner_web_view().eval_js('''(function(){{
+    return len(get_cell_ids(widget, filter_function))
+
+
+def get_cell_ids(widget, filter_function):
+    """
+    :param qmxgraph.widget.QmxGraph widget: The widget used to display the
+        graph.
+    :param str filter_function: A javascript function used to filter the
+        cells.
+    :rtype: list[str]
+    """
+    cells_ids = widget.inner_web_view().eval_js('''(function(){{
         var all_cells = api._graphEditor.graph.model.cells;
         var cells = Object.keys(all_cells).map(
             function(id){{ return all_cells[id]; }}
         );
 
         cells = cells.filter({filter_func});
-        return cells.length;
+        return cells.map(function(aCell){{ return aCell.getId(); }});
     }})()'''.format(filter_func=filter_function))
-    return int(count)
+    return cells_ids
