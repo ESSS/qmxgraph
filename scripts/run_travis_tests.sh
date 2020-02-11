f4c57592a57befe25f2d19c86061c491bffede63
Martin Prüsse <prusse@esss.co>
2020-02-11 14:28:43 -0300

    Use mxgraph v4.0.6 on travis CI

--- scripts/run_travis_tests.sh
+++ scripts/run_travis_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone --depth=1 https://github.com/jgraph/mxgraph.git
+git clone --depth=1 --branch v4.0.6 https://github.com/jgraph/mxgraph.git
 export PYTEST_ADDOPTS="--driver PhantomJS"
 export MXGRAPHPATH=mxgraph
 inv qrc
