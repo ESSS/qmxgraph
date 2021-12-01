8659e25b41046e3dbf92e65a4622275d386e7db3
Martin Prüsse <prusse.martin@gmail.com>
2021-12-01 11:43:48 -0300

    Update docs generation

--- src/qmxgraph/__init__.py
+++ src/qmxgraph/__init__.py
@@ -1,4 +1,10 @@
+import os
+
 # This import is placed on the qmxgraph's `__init__` module to make it
 # less likely users to stumble into the "ImportError: QtWebEngineWidgets
 # must be imported before a QCoreApplication instance is created" error.
-import PyQt5.QtWebEngineWidgets  # noqa
+#
+# Some ugliness to successfully build the doc on ReadTheDocs...
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if not on_rtd:
+    import PyQt5.QtWebEngineWidgets  # noqa
