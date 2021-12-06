1fb0ac429f285d623ab40eb4f13e2f88b378f9f5
Mauricio Oliveira Haensch <oliveira@esss.com.br>
2021-12-06 15:15:06 -0300

    Add a couple cases to setStyleKey and removeStyleKey tests

--- tests/test_js_utils.py
+++ tests/test_js_utils.py
@@ -9,6 +9,7 @@ def graph(graph_cases: GraphCaseFactory) -> BaseGraphCase:
 
 
 def test_set_style_key(graph: BaseGraphCase) -> None:
+    assert graph.eval_js_function('graphs.utils.setStyleKey', '', 'bar', 5) == 'bar=5'
     assert graph.eval_js_function('graphs.utils.setStyleKey', 'style', 'bar', 5) == 'style;bar=5'
     assert graph.eval_js_function('graphs.utils.setStyleKey', 'style;bar=7', 'bar', 5) == 'style;bar=5'
     assert graph.eval_js_function('graphs.utils.setStyleKey', 'foobar=3;bar=7', 'bar', 5) == 'foobar=3;bar=5'
@@ -17,6 +18,7 @@ def test_set_style_key(graph: BaseGraphCase) -> None:
 
 
 def test_remove_style_key(graph: BaseGraphCase) -> None:
+    assert graph.eval_js_function('graphs.utils.removeStyleKey', '', 'bar') == ''
     assert graph.eval_js_function('graphs.utils.removeStyleKey', 'style;bar=7', 'bar') == 'style'
     assert graph.eval_js_function('graphs.utils.removeStyleKey', 'style;bar=7', 'foo') == 'style;bar=7'
     assert graph.eval_js_function('graphs.utils.removeStyleKey', 'style;bar=7', 'style') == 'style;bar=7'
