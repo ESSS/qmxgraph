f72582afddf4cd215f4d5457c9a503aa150d1da2
Bruno Oliveira <bruno@esss.com.br>
2021-12-10 08:05:53 -0300

    Add prettier pre-commit hook to auto format JS (#118)
    Co-authored-by: Bruno Oliveira <bruno@esss.co>

--- src/qmxgraph/page/fixes.js
+++ src/qmxgraph/page/fixes.js
@@ -2,10 +2,9 @@
  * These are some polyfils or fixes.
  */
 
-
 // In Qt 5.6.2 String.prototype.lastIndexOf is broken.
 // https://bugreports.qt.io/browse/QTBUG-54287
-if ('asd'.lastIndexOf('s') !== 1) {
+if ("asd".lastIndexOf("s") !== 1) {
     /**
      * See: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
      * This implementation uses:
@@ -14,12 +13,12 @@ if ('asd'.lastIndexOf('s') !== 1) {
      * - String.prototype.substr
      * - parseInt
      */
-    String.prototype.lastIndexOf = function lastIndexOf (searchValue, fromIndex) {
+    String.prototype.lastIndexOf = function lastIndexOf(searchValue, fromIndex) {
         "use strict";
         searchValue = String(searchValue);
         var searchValueLength = searchValue.length;
         var thisLength = this.length;
-        if (typeof fromIndex === 'undefined') {
+        if (typeof fromIndex === "undefined") {
             fromIndex = thisLength - searchValueLength;
         } else {
             fromIndex = parseInt(fromIndex, 10) || 0;
