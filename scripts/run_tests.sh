6d5dcda5f1ed937cef5102082bc086929867ab00
Martin Prüsse <prusse@esss.co>
2023-11-08 18:40:13 -0300

    Bypass issues with video card drivers
    ASIM-5273

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 git clone --depth=1 --branch v3.7.5 https://github.com/jgraph/mxgraph.git
 export MXGRAPHPATH=mxgraph
+
+export LIBGL_ALWAYS_SOFTWARE=1
+export GALLIUM_DRIVER=llvmpipe
+export ENABLE_LLVMPIPE_GL=1
+export QT_QUICK_BACKEND=software
+export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --no-sandbox"
+
 inv qrc
 inv test
