476e51d09759f05eab8949a4783c78cd229c0e7b
Bruno Oliveira <bruno@esss.com.br>
2024-11-11 15:09:35 -0300

    Drop Python 3.8 and 3.9 support (#217)

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Visualization",
         "Topic :: Software Development :: User Interfaces",
